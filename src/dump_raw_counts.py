aea07ce09bbf972746f044b1b37bf598cc1f51ed
Ruben Giro <rgiro@andrew.cmu.edu>
2023-10-29 23:48:08 -0400

    week1

--- src/dump_raw_counts.py
+++ src/dump_raw_counts.py
@@ -1,8 +1,18 @@
 import duckdb
-#duckdb.sql('SELECT 42').show()
+
 
 con = duckdb.connect('main.db')
 
-a = con.execute("""select count (*) from bikedata""")
+df = con.execute('DESCRIBE').df().name.to_list()
 
-print("ok")
+with open('raw_counts.txt', 'w') as file:
+    for table in df:
+        sql_q = f'SELECT COUNT(*) FROM {table}' 
+        a = con.execute(sql_q).df().iloc[0,0]
+        print(table, a)
+        file.write(table +' ' + str(a) + '\n')
+
+
+file.close()
+
+
