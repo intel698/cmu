43f3410b0120fa98531f6905e107dc91291c06ab
Ruben Giro <rgiro@andrew.cmu.edu>
2023-11-03 20:00:28 -0400

    ingest
    ingest

--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,12 +1,12 @@
-CREATE TABLE central_park_weather AS FROM read_csv_auto('central_park_weather.csv');
-CREATE TABLE fhv_bases AS FROM read_csv_auto('fhv_bases.csv');
-CREATE TABLE bikedata AS FROM read_csv_auto('citibike-tripdata.csv');
+-- load location & weather data from CSV files, merging columns by name and storing as strings
+create table fhv_bases as select * from  read_csv_auto('./data/fhv_bases.csv', union_by_name=True, filename=True, all_varchar=1, header=True);
+create table central_park_weather as select * from read_csv_auto('./data/central_park_weather.csv', union_by_name=True, filename=True, all_varchar=1);
 
+-- load taxi data from parquet files, merging columns by name
+create table yellow_tripdata as select * from read_parquet('./data/taxi/yellow_tripdata.parquet', union_by_name=True, filename=True);
+create table green_tripdata as select * from read_parquet('./data/taxi/green_tripdata.parquet', union_by_name=True, filename=True);
+create table fhvhv_tripdata as select * from read_parquet('./data/taxi/fhvhv_tripdata.parquet', union_by_name=True, filename=True);
+create table fhv_tripdata as select * from read_parquet('./data/taxi/fhv_tripdata.parquet', union_by_name=True, filename=True);
 
-CREATE TABLE fhv_tripdata AS SELECT * FROM read_parquet('taxi/fhv_tripdata.parquet');
-
-CREATE TABLE fhvhv_tripdata.parquet AS SELECT * FROM read_parquet('fhvhv_tripdata.parquet');
-
-CREATE TABLE green_tripdata.parquet AS SELECT * FROM read_parquet('green_tripdata.parquet');
-
-CREATE TABLE yellow_tripdata.parquet AS SELECT * FROM read_parquet('yellow_tripdata.parquet');
+-- load bike data from CSV files, merging columns by name and storing as strings
+create table bike_data as select * from read_csv_auto('./data/citibike-tripdata.csv.gz', union_by_name=True, filename=True, all_varchar=1);
